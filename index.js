1840fd3ee55e82a53f28560ae76c1b77a0c5ee1f
victor ogbonna <victorgbonna@gmail.com>
2020-10-06 16:22:32 +0100

    session express way

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express= require('express')
+const session=require('express-session')
 const bodyParser= require('body-parser')
 const authRoute= require('./routes/authRoute')
 const User= require('./modules/users/models/user')
@@ -8,9 +9,26 @@ const app=express()
 app.use(bodyParser.urlencoded({extended:false}))
 app.set('view engine', 'ejs')
 
+// app.set('trust proxy',1)
+app.use(session({
+    secret: '642a3f8e31251692cd18d68281314984318abd3b',
+    resave: false,
+    saveUninitialized:true,
+    cookie:{secure:false}
+}))
+
 app.use('/', authRoute)
 
 app.get('/', (req,res)=>{
+    // console.log(req.session)
+    // if(!req.session.name){
+    //     req.session.name='John'
+    // }
+    // else{   
+    //     console.log('Name', req.session.name)
+    // }
+    req.session.views=(req.session.views || 0) + 1
+    console.log(req.session.views)
     return res.render('index')
 })
 
