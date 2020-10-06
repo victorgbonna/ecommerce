6546c7be37ca33fd82538be9124c02239f79cc4e
victor ogbonna <victorgbonna@gmail.com>
2020-10-06 16:30:44 +0100

    Nodemon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const app=express()
 app.get('/', (req,res)=>{
-    return res.send('Hello');
+    return res.send('Hello j');
 })
 app.listen(3000,()=>{
     console.log('Server running on port 3000');
