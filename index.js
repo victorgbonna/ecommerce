35224f081270f18e20dae15d60ad11b317cea1f4
victor ogbonna <victorgbonna@gmail.com>
2020-10-06 16:25:05 +0100

    ejs on the go

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express= require('express')
 const app=express()
+
+
+app.set('view engine', 'ejs')
+
+
+
 app.get('/', (req,res)=>{
-    return res.send('Hello World!')
+    return res.render('index')
 })
 app.listen(3000,()=>{
     console.log('Server running on port 3000');
